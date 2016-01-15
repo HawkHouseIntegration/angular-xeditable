52165b93d6c8c866a3e641a386fc948706519532
Alexander Surinov <a.surinov@h-h-i.ru>
2016-01-15 15:29:50 +0300

    custom CheckBox

--- starter/angular-xeditable/js/xeditable.js
+++ starter/angular-xeditable/js/xeditable.js
@@ -484,6 +484,27 @@ angular.module('xeditable').directive('editableCheckbox', ['editableDirectiveFac
     });
 }]);
 
+angular.module('xeditable').directive('editableCheckboxCustom', ['editableDirectiveFactory',
+  function(editableDirectiveFactory) {
+    return editableDirectiveFactory({
+      directiveName: 'editableCheckboxCustom',
+      inputTpl: '<div class="checkbox"><label><input type="checkbox" ng-model="$data"><span ng-bind="$data"></span></label></div>',
+      render: function() {
+        this.parent.render.call(this);
+      },
+      autosubmit: function() {
+        var self = this;
+        self.inputEl.bind('change', function() {
+          setTimeout(function() {
+            self.scope.$apply(function() {
+              self.scope.$form.$submit();
+            });
+          }, 500);
+        });
+      }
+    });
+  }]);
+
 // checklist
 angular.module('xeditable').directive('editableChecklist', [
   'editableDirectiveFactory',
